b718e529af397d092460c7c993049ec5a0b76129
Yuliia <yuliapysarenko@gmail.com>
2024-07-04 16:16:25 +0300

    Fix authorization

--- src/utils/firebase/helpers.js
+++ src/utils/firebase/helpers.js
@@ -12,50 +12,21 @@ export const getUIConfig = (setUser) => {
   return {
     callbacks: {
       signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-        // Do something with the returned AuthResult.
-        // Return type determines whether we continue the redirect
-        // automatically or whether we leave that to developer to handle.
-
         if (authResult.additionalUserInfo.profile) {
           const { displayName, uid } = authResult.user;
           setUser({ displayName, uid });
         }
         return false;
       },
-      signInFlow: "popup",
       signInFailure: function (error) {
-        // Some unrecoverable error occurred during sign-in.
-        // Return a promise when error handling is completed and FirebaseUI
-        // will reset, clearing any UI. This commonly occurs for error code
-        // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
-        // occurs. Check below for more details on this.
         return handleUIError(error);
       },
-      // uiShown: function() {
-      //   // The widget is rendered.
-      //   // Hide the loader.
-      //   document.getElementById('loader').style.display = 'none';
-      // }
     },
+    signInFlow: "popup",
     signInSuccessUrl: "/",
     signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      // firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
-    // tosUrl and privacyPolicyUrl accept either url string or a callback
-    // function.
-    // Terms of service url/callback.
-    // tosUrl: '<your-tos-url>',
-    // Privacy policy url/callback.
-    // privacyPolicyUrl: function() {
-    //   window.location.assign('<your-privacy-policy-url>');
-    // }
   };
 };
 
